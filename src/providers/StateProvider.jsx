65c7b50d8594f4214920a9a89e4971c192f72833
eduard-lab-front-end <eduard-lab@web.de>
2024-10-04 12:21:13 +0200

    Quick Fix

--- src/providers/StateProvider.jsx
+++ src/providers/StateProvider.jsx
@@ -4,8 +4,6 @@ export const ProductContext = createContext();
 
 const ProductProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-  const [cartData, setCartData] = useState([]);
-  const [favoriteData, setFavoriteData] = useState([]);
   const [needRefresh, setNeedRefresh] = useState(true);
 
   const fetchProducts = async () => {
@@ -21,38 +19,10 @@ const ProductProvider = ({ children }) => {
       console.error("Error getting products", error);
     }
   };
-  const fetchCartProducts = async () => {
-    try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/cart`);
-      if (response.ok) {
-        const data = await response.json();
-        setCartData(data);
-      } else if (response === 500) {
-        alert("Already in your cart");
-      }
-    } catch (error) {
-      console.error("Error getting products", error);
-    }
-  };
-  const fetchFavoriteProducts = async () => {
-    try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/favorite`);
-      if (response.ok) {
-        const data = await response.json();
-        setFavoriteData(data);
-      } else if (response === 500) {
-        alert("Already in your cart");
-      }
-    } catch (error) {
-      console.error("Error getting products", error);
-    }
-  };
- 
+
   useEffect(() => {
     if (needRefresh) {
       fetchProducts();
-      fetchCartProducts();
-      fetchFavoriteProducts();
       setNeedRefresh(false);
     }
   }, [needRefresh]);
@@ -61,8 +31,6 @@ const ProductProvider = ({ children }) => {
     <ProductContext.Provider
       value={{
         products,
-        cartData,
-        favoriteData,
         fetchProducts,
         setNeedRefresh,
       }}
